7940a5dc74efaebf2aff27d4c89a5fa7dce425db
Rishabh <rishabharora1780@gmail.com>
2025-11-01 12:52:40 +0530

    Revert "changes in vite config."
    This reverts commit 8ce44c79faf072a39f5ca0b081d598ce7b247ec4.

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  // Use absolute base for custom domain so nested routes load assets
-  base: '/',
+  // Use relative base for GitHub Pages
+  base: './',
   plugins: [react()],
 })
 
